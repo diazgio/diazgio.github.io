2679cf753604d3095f8d18ef117f7ba8043208b4
Giordano Dìaz <diazgiordano@gmail.com>
2021-03-16 20:06:48 -0500

    Change projects section from modals to sliders

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,19 +1,52 @@
+// main slider --------------------------
+let mSliderIndex = 1;
+showSlidesMain(mSliderIndex);
+
+function plusSlidesMain(n) {
+  showSlidesMain(mSliderIndex += n);
+}
+
+function currentSlideMain(n) {
+  showSlidesMain(mSliderIndex = n);
+}
+
+function showSlidesMain(n) {
+  var i;
+  var slides = document.getElementsByClassName('proj-card');
+  if (n > slides.length) {mSliderIndex = 1}
+    if (n < 1) {mSliderIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+  slides[mSliderIndex - 1].style.display = 'block';
+}
+// Js capstone Slider --------------
+
+let JslideIndex = 1;
+showSlidesJs(JslideIndex);
+
+function currentSlideJs(n) {
+  showSlidesJs(JslideIndex = n);
+}
+
+function showSlidesJs(n) {
+  var i;
+  var slides = document.getElementsByClassName('projSlides0');
+  var dots = document.getElementsByClassName('jscapstone-dot');
+  if (n > slides.length) {JslideIndex = 1}
+    if (n < 1) {JslideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+  slides[JslideIndex - 1].style.display = 'block';
+  dots[JslideIndex - 1].className += ' active';
+}
+
+
 // no-pro modal and Slider --------------
-var pmodal = document.getElementById('no-proModal');
-var pbtn = document.getElementById('no-proBtn');
-var pspan = document.getElementsByClassName('no-problem-close')[0];
-
-// Get the modal -------------------
-
-// When user clicks on the button, open the modal
-pbtn.onclick = function() {
-  pmodal.style.display = 'block';
-}
-
-// When the user clicks on (x), close the modal
-pspan.onclick = function() {
-  pmodal.style.display = 'none'
-}
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -28,7 +61,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName('no-proSlides');
+  var slides = document.getElementsByClassName('projSlides');
   var dots = document.getElementsByClassName('no-pro-dot');
   if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
@@ -42,29 +75,9 @@ function showSlides(n) {
   dots[slideIndex - 1].className += ' active';
 }
 
-// Cosplay modal and Slider --------------
-var cmodal = document.getElementById('cosplayModal');
-var cbtn = document.getElementById('cosplayBtn');
-var cspan = document.getElementsByClassName('cosplay-close')[0];
-
-// Get the modal -------------------
-
-// When user clicks on the button, open the modal
-cbtn.onclick = function() {
-  cmodal.style.display = 'block';
-}
-
-// When the user clicks on (x), close the modal
-cspan.onclick = function() {
-  cmodal.style.display = 'none'
-}
-
+// // Cosplay modal and Slider --------------
 let cslideIndex = 1;
 showSlidesC(cslideIndex);
-
-function plusSlidesC(n) {
-  showSlidesC(cslideIndex += n);
-}
 
 function currentSlideC(n) {
   showSlidesC(cslideIndex = n);
@@ -72,7 +85,7 @@ function currentSlideC(n) {
 
 function showSlidesC(n) {
   var i;
-  var slides = document.getElementsByClassName('cosplaySlides');
+  var slides = document.getElementsByClassName('projSlides2');
   var dots = document.getElementsByClassName('cosplay-dot');
   if (n > slides.length) {cslideIndex = 1}
     if (n < 1) {cslideIndex = slides.length}
@@ -85,179 +98,96 @@ function showSlidesC(n) {
   slides[cslideIndex - 1].style.display = 'block';
   dots[cslideIndex - 1].className += ' active';
 }
-
-// Consultly modal and Slider --------------
-var comodal = document.getElementById('consultlyModal');
-var cobtn = document.getElementById('consultlyBtn');
-var cospan = document.getElementsByClassName('consultly-close')[0];
-
-// Get the modal -------------------
-
-// When user clicks on the button, open the modal
-cobtn.onclick = function() {
-  comodal.style.display = 'block';
-}
-
-// When the user clicks on (x), close the modal
-cospan.onclick = function() {
-  comodal.style.display = 'none'
-}
-
-let coslideIndex = 1;
-showSlidesCo(coslideIndex);
-
-function plusSlidesCo(n) {
-  showSlidesCo(coslideIndex += n);
-}
+// // Consultly modal and Slider --------------
+let colideIndex = 1;
+showSlidesCo(colideIndex);
 
 function currentSlideCo(n) {
-  showSlidesCo(coslideIndex = n);
+  showSlidesCo(colideIndex = n);
 }
 
 function showSlidesCo(n) {
   var i;
-  var slides = document.getElementsByClassName('consultlySlides');
+  var slides = document.getElementsByClassName('projSlides3');
   var dots = document.getElementsByClassName('consultly-dot');
-  if (n > slides.length) {coslideIndex = 1}
-    if (n < 1) {coslideIndex = slides.length}
+  if (n > slides.length) {colideIndex = 1}
+    if (n < 1) {colideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none';
     }
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(' active', '');
     }
-  slides[coslideIndex - 1].style.display = 'block';
-  dots[coslideIndex - 1].className += ' active';
+  slides[colideIndex - 1].style.display = 'block';
+  dots[colideIndex - 1].className += ' active';
 }
 
-// Memorize modal and Slider --------------
-var memodal = document.getElementById('memorizeModal');
-var mebtn = document.getElementById('memoBtn');
-var mespan = document.getElementsByClassName('memo-close')[0];
+// // Memorize Slider --------------
+let melideIndex = 1;
+showSlidesMe(melideIndex);
 
-// Get the modal -------------------
-
-// When user clicks on the button, open the modal
-mebtn.onclick = function() {
-  memodal.style.display = 'block';
+function currentSlideMe(n) {
+  showSlidesMe(melideIndex = n);
 }
 
-// When the user clicks on (x), close the modal
-mespan.onclick = function() {
-  memodal.style.display = 'none'
-}
-
-let meslideIndex = 1;
-showSlidesM(meslideIndex);
-
-function plusSlidesM(n) {
-  showSlidesM(meslideIndex += n);
-}
-
-function currentSlideM(n) {
-  showSlidesM(meslideIndex = n);
-}
-
-function showSlidesM(n) {
+function showSlidesMe(n) {
   var i;
-  var slides = document.getElementsByClassName('memoSlides');
-  var dots = document.getElementsByClassName('memo-dot');
-  if (n > slides.length) {meslideIndex = 1}
-    if (n < 1) {meslideIndex = slides.length}
+  var slides = document.getElementsByClassName('projSlides4');
+  var dots = document.getElementsByClassName('memorize-dot');
+  if (n > slides.length) {melideIndex = 1}
+    if (n < 1) {melideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none';
     }
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(' active', '');
     }
-  slides[meslideIndex - 1].style.display = 'block';
-  dots[meslideIndex - 1].className += ' active';
+  slides[melideIndex - 1].style.display = 'block';
+  dots[melideIndex - 1].className += ' active';
+}
+// // SDP modal and Slider --------------
+let sdlideIndex = 1;
+showSlidesSd(sdlideIndex);
+
+function currentSlideSd(n) {
+  showSlidesSd(sdlideIndex = n);
 }
 
-// SDP modal and Slider --------------
-var smodal = document.getElementById('sdpModal');
-var sbtn = document.getElementById('sdpBtn');
-var sspan = document.getElementsByClassName('sdp-close')[0];
-
-// Get the modal -------------------
-
-// When user clicks on the button, open the modal
-sbtn.onclick = function() {
-  smodal.style.display = 'block';
-}
-
-// When the user clicks on (x), close the modal
-sspan.onclick = function() {
-  smodal.style.display = 'none'
-}
-
-let sslideIndex = 1;
-showSlidesS(sslideIndex);
-
-function plusSlidesS(n) {
-  showSlidesS(sslideIndex += n);
-}
-
-function currentSlideS(n) {
-  showSlidesS(sslideIndex = n);
-}
-
-function showSlidesS(n) {
+function showSlidesSd(n) {
   var i;
-  var slides = document.getElementsByClassName('sdpSlides');
+  var slides = document.getElementsByClassName('projSlides5');
   var dots = document.getElementsByClassName('sdp-dot');
-  if (n > slides.length) {sslideIndex = 1}
-    if (n < 1) {sslideIndex = slides.length}
+  if (n > slides.length) {sdlideIndex = 1}
+    if (n < 1) {sdlideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none';
     }
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(' active', '');
     }
-  slides[sslideIndex - 1].style.display = 'block';
-  dots[sslideIndex - 1].className += ' active';
+  slides[sdlideIndex - 1].style.display = 'block';
+  dots[sdlideIndex - 1].className += ' active';
+}
+// // MusicBot modal and Slider --------------
+let mcslideIndex = 1;
+showSlidesMS(mcslideIndex);
+
+function currentSlideMs(n) {
+  showSlidesMS(mcslideIndex = n);
 }
 
-// MusicBot modal and Slider --------------
-var bmodal = document.getElementById('musicModal');
-var bbtn = document.getElementById('musicBtn');
-var bspan = document.getElementsByClassName('music-close')[0];
-
-// Get the modal -------------------
-
-// When user clicks on the button, open the modal
-bbtn.onclick = function() {
-  bmodal.style.display = 'block';
-}
-
-// When the user clicks on (x), close the modal
-bspan.onclick = function() {
-  bmodal.style.display = 'none'
-}
-
-let bslideIndex = 1;
-showSlidesB(bslideIndex);
-
-function plusSlidesB(n) {
-  showSlidesB(bslideIndex += n);
-}
-
-function currentSlideB(n) {
-  showSlidesB(bslideIndex = n);
-}
-
-function showSlidesB(n) {
+function showSlidesMS(n) {
   var i;
-  var slides = document.getElementsByClassName('musicSlides');
+  var slides = document.getElementsByClassName('projSlides6');
   var dots = document.getElementsByClassName('music-dot');
-  if (n > slides.length) {bslideIndex = 1}
-    if (n < 1) {bslideIndex = slides.length}
+  if (n > slides.length) {mcslideIndex = 1}
+    if (n < 1) {mcslideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none';
     }
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(' active', '');
     }
-  slides[bslideIndex - 1].style.display = 'block';
-  dots[bslideIndex - 1].className += ' active';
+  slides[mcslideIndex - 1].style.display = 'block';
+  dots[mcslideIndex - 1].className += ' active';
 }
