d92702b9387248af1d2d43768eb419eef9fca519
Giordano Díaz <diazgiordano@gmail.com>
2022-08-24 15:26:44 -0500

    Skills component finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import RubyonRails from '../assets/rubyonrails.png';
+import Docker from '../assets/docker.png';
+import GitHub from '../assets/github.png';
+import Postgresql from '../assets/postgresql.png';
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+            <p className='py-4'>These are the technologies I've worked with</p>
+        </div>
+
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+            <p className='my-4'>HTML</p>
+          </div>
+
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+            <p className='my-4'>CSS</p>
+          </div>
+
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={JavaScript} alt="JS icon" />
+            <p className='my-4'>JAVASCRIPT</p>
+          </div>
+
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={RubyonRails} alt="ROR icon" />
+            <p className='my-4'>RUBY ON RAILS</p>
+          </div>
+
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Postgresql} alt="POSTGRES icon" />
+            <p className='my-4'>POSTGRESQL</p>
+          </div>
+
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={ReactImg} alt="REACT icon" />
+            <p className='my-4'>REACT</p>
+          </div>
+
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={GitHub} alt="GITHUB icon" />
+            <p className='my-4'>GITHUB</p>
+          </div>
+
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Docker} alt="DOCKER icon" />
+            <p className='my-4'>DOCKER</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills;
