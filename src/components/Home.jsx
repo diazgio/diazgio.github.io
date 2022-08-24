e0c6d42a6b7c5174e9ffd89ede597e21a1a085bf
Giordano Díaz <diazgiordano@gmail.com>
2022-08-24 14:50:44 -0500

    Finish Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,30 @@ import { HiArrowNarrowRight } from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
-
+    <div name="home" className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-[#D2D621]'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Giordano Díaz
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          I'm a Full Stack Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          I’m a full-stack developer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I’m focused on
+          building responsive full-stack web applications.
+        </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#D2D621] hover:border-[#D2D621] hover:text-black'>
+            View Work
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3'/>
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
